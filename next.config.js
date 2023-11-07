c04612e071a227a040ec3ab0ff624d106ca4d420
mootez dakhlaoui <m.daklaui@yahoo.com>
2023-11-08 00:02:16 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,16 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'res.cloudinary.com',
-            // You can add these as well
-            // port: '',
-            // pathname: 'arifscloud/image/upload/**',
-          },
-        ],
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        // You can add these as well
+        // port: '',
+        // pathname: 'arifscloud/image/upload/**',
       },
+    ],
+  },
 }
 
 module.exports = nextConfig
