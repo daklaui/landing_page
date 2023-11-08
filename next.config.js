5673ec4d5adab879258b3e84a1dbea484ec126cd
mootez dakhlaoui <m.daklaui@yahoo.com>
2023-11-08 14:15:59 +0100

    .

--- next.config.js
+++ next.config.js
@@ -9,9 +9,6 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
-        // You can add these as well
-        // port: '',
-        // pathname: 'arifscloud/image/upload/**',
       },
     ],
   },
